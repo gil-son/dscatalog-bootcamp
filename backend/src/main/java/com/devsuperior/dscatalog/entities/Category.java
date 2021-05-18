77c62a3abeea69588542fda8df370eb070d67dbc
gil-son <gilszon@gmail.com>
2021-05-18 19:23:06 -0300

    CategoryResource

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,64 @@
+package com.devsuperior.dscatalog.entities;
+
+import java.io.Serializable;
+
+public class Category implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	
+	public Category() {
+		
+	}
+
+	public Category(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// hashCode will Compare objects, but there is a small possibility that two or more objects will generate the same number
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	// equals is slower than hashCode, but ensures that there is no equality
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+// So because the hashCode is faster, it is used. And, if there are identical objects, equals is used to complement
+}
