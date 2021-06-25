d8ca7e4c05abc804e6c90fa7dbcecfeba809abee
gil-son <gilszon@gmail.com>
2021-06-24 21:24:43 -0300

    Repository Test

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -3,7 +3,9 @@ package com.devsuperior.dscatalog.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dscatalog.entities.Product;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository  extends JpaRepository<Product, Long>{
 
 }
---
+++ backend/src/test/java/repositories/ProductRepositoryTests.java
@@ -0,0 +1,29 @@
+package repositories;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+	@Autowired
+	public ProductRepository repository;
+		
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExists() {
+		
+		Long existingId = 1L;
+		
+		repository.deleteById(existingId);
+		
+		Optional<Product> result = repository.findById(existingId);
+		Assertions.assertFalse(result.isPresent());
+	}
+}
