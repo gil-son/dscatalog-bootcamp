dad85ab54b290d765baf827fcea8aa6c4b699ac7
gil-son <gilszon@gmail.com>
2021-06-25 21:44:25 -0300

    Organized

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,29 +1,44 @@
-package repositories;
+package com.devsuperior.dscatalog.repositories;
 
 import java.util.Optional;
 
-import org.junit.Test;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuperior.dscatalog.entities.Product;
-import com.devsuperior.dscatalog.repositories.ProductRepository;
+
 
 @DataJpaTest
 public class ProductRepositoryTests {
 	
 	@Autowired
-	public ProductRepository repository;
-		
+	private ProductRepository repository;
+
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
 		
-		Long existingId = 1L;
+		long existingId = 1L;
 		
 		repository.deleteById(existingId);
 		
 		Optional<Product> result = repository.findById(existingId);
 		Assertions.assertFalse(result.isPresent());
 	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+		
+		long nonExistingId = 1000L;
+							
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			repository.deleteById(nonExistingId);
+		});
+		
+	}
+	
+	
 }
