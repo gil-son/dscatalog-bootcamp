2e7ca178a1527124c85ef2c509855afce0f4e271
“gil-son” <gilszon@gmail.com>
2021-09-03 20:18:07 -0300

    Integration test for findAllPaged

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
@@ -2,12 +2,15 @@ package com.devsuperior.dscatalog.services;
 
 import javax.transaction.Transactional;
 
-import org.junit.Test;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
@@ -15,32 +18,55 @@ import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 // In Integration Test, the application will access database
 @SpringBootTest
-@Transactional // 
+@Transactional // Reset data after any test. This will create an idependence to each test
 public class ProductServiceIT { // Integration Test
-
+	
+	@Autowired
 	private ProductService service;
+	
+	@Autowired
 	private ProductRepository repository;
 	
 	private Long existingId;
 	private Long nonExistingId;
 	private Long countTotalProducts;
 	
+		
 	
-	ProductServiceIT(){}
-	
-	ProductServiceIT(ProductService service){
-		this.service = service;
-	}
-	
-	ProductServiceIT(ProductRepository repository){
-		this.repository = repository;
-	}
 	
 	@BeforeEach
 	void setUp() throws Exception{
 		existingId = 1L;
 		nonExistingId = 1000L;
 		countTotalProducts = 25L;
+	}
+	
+	  
+	
+	@Test
+	public void findAllPagedShouldReturnSortedPageWhenSortByName() {
+		
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
+	}
+	
+	
+	
+	@Test
+	public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExist() {
+		
+		PageRequest pageRequest = PageRequest.of(50, 10);
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertTrue(result.isEmpty());
+		
 	}
 	
 	
