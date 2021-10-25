dfae79ad61c97472b25ea48bb02a21fd651bd3ab
“gil-son” <gilszon@gmail.com>
2021-10-25 18:51:19 -0300

    Custom validation response

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ValidationError.java
@@ -0,0 +1,19 @@
+package com.devsuperior.dscatalog.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	List<FieldMessage> listErrors = new ArrayList<>();
+	
+	public List<FieldMessage> getListErrors() {
+		return listErrors;
+	}
+
+	public void addErrors(String fieldError, String message) {
+		listErrors.add(new FieldMessage(fieldError, message));
+	}
+	
+}
