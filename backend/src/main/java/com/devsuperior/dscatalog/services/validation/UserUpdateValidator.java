c10950e5c033ea1cbd48238f5ec37827b906acd7
“gil-son” <gilszon@gmail.com>
2021-10-30 11:50:05 -0300

    UserUpdateValidator

--- backend/src/main/java/com/devsuperior/dscatalog/services/validation/UserUpdateValidator.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/validation/UserUpdateValidator.java
@@ -2,29 +2,43 @@ package com.devsuperior.dscatalog.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
-import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.dto.UserUpdateDTO;
 import com.devsuperior.dscatalog.entities.User;
 import com.devsuperior.dscatalog.repositories.UserRepository;
 import com.devsuperior.dscatalog.resources.exceptions.FieldMessage;
 
-public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserDTO> {
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+	
+	
+	@Autowired
+	private HttpServletRequest request; // Contains the information from request
 	
 	
 	@Autowired
 	UserRepository userRepository;
 	
 	@Override // Logic used when initialize the object
-	public void initialize(UserInsertValid ann) {
+	public void initialize(UserUpdateValid ann) {
 	}
 
 	@Override
-	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+	public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
+		
+		// In Http, all is string and this case is key and value, then is use the Cast
+		
+		@SuppressWarnings("unchecked") // remove the yellow mark
+		var uri = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE); // Get a dictionary with attributes
+		long userId = Long.parseLong(uri.get("id"));
+		
 		
 		List<FieldMessage> list = new ArrayList<>();
 		
@@ -32,7 +46,7 @@ public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid,
 		
 		User user = userRepository.findByEmail(dto.getEmail());
 		
-		if(user != null) {
+		if(user != null && user.getId() != userId) {
 			list.add(new FieldMessage("email","The email already exist!"));
 		}
 		
